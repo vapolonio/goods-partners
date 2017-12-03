80118fa423055ef911d78ec4534f4da6c609f5ca
vapolonio <victor@apolon.io>
2017-12-03 08:46:05 -0200

    added model classifier

---
+++ controller/runClassifier.js
@@ -0,0 +1,9 @@
+// import PythonShell from 'python-shell';
+
+// export function runModel(){
+//     PythonShell.run('../models/classifier.py', function (err) {
+//         if (err) throw err;
+//         console.log('finished');
+//     });
+// }
+
---
+++ routes/model.js
@@ -0,0 +1,12 @@
+// import express from 'express'
+
+// let path = require('path');
+// let request = require('superagent');
+// import { runModel } from '../controller/runClassifier';
+// var router = express.Router();
+
+// router.get('/', (req, res) => {
+//     runModel();
+//     // let url = `${auth_url}/OriginalConnect?scopes=account&callback_url=${auth_callback_url}&callback_id=1&developer_key=${developer_key}`;
+//     // res.redirect(url);
+// });
