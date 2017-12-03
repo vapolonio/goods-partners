dd9c7e07a4a6e0b16471c4d08e8eef8c188310f0
vinnimin <vini.min@hotmail.com>
2017-12-03 09:47:57 -0200

    commit

--- controller/runClassifier.js
+++ controller/runClassifier.js
@@ -1,9 +1,9 @@
-// import PythonShell from 'python-shell';
+import PythonShell from 'python-shell';
 
-// export function runModel(){
-//     PythonShell.run('../models/classifier.py', function (err) {
-//         if (err) throw err;
-//         console.log('finished');
-//     });
-// }
+export function runModel(){
+    PythonShell.run('../models/classifier.py', function (err) {
+        if (err) throw err;
+        console.log('finished');
+    });
+}
 
