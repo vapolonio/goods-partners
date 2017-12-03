6b5b0b4e74d457bff04f8054004c3c58be118a9b
vinnimin <vini.min@hotmail.com>
2017-12-03 02:44:50 -0200

    commit

--- controller/originalApiController.js
+++ controller/originalApiController.js
@@ -12,48 +12,15 @@ let developer_key = '28f955c90b3a2940134ff1a970050f569a87facf';
 let secret_key = 'dd385cd0b59c013560400050569a7fac';
 let access_token = '';
 
-let resources = {
-    balance: {
-        method: 'get',
-        path: '/accounts/v1/balance'
-    },
-    balance_history: {
-        method: 'get',
-        path: '/accounts/v1/balance-history?date_from=20170623'
-    },
-    history: {
-        method: 'get',
-        path: '/accounts/v1/transaction-history'
-    }
+let api = {};
+
+api.setAccessToken = function(token){
+    access_token = token;
+} 
+
+api.getAccessToken = function(){
+    return access_token;
 }
 
-let execute_api = name => {
-    let resource = resources[name];
-    
-    let action =
-        request
-            [resource.method](`${api_url}${resource.path}`)
-            .set('developer-key', developer_key)
-            .set('Authorization', access_token);
 
-    if ('headers' in resource)
-        for (let key in resource.headers)
-            action.set(key, resource.headers[key]);
-
-    if ('data' in resource)
-        action.send(resource.data);
-
-
-    action.end((err, res) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(res.body);
-
-            if ('security_message' in res.body) {
-                resources.tef_confirm.headers.security_response = res.body.security_message
-            }
-        }
-    });
-};
+module.exports = api;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import express from 'express'
 
 /* GET home page. */
 let request = require('superagent');
+let originalApi = require('../controller/originalApiController');
 var router = express.Router();
 
 const url = `http://localhost:3000/`;
@@ -12,11 +13,10 @@ let auth_url = 'https://sb-autenticacao-api.original.com.br';
 let auth_callback_url = `http://localhost:3000/callback`
 let developer_key = '28f955c90b3a2940134ff1a970050f569a87facf';
 let secret_key = 'dd385cd0b59c013560400050569a7fac';
-let access_token = '';
 
 router.get('/', function(req, res, next) {
-    // res.status(200).json({ data: 'Hello World' });
-    res.redirect('/oauth');
+    res.status(200).json({ data: 'Hello World' });
+    // res.redirect('/oauth');
 });
 
 router.get('/favicon.ico', function(req, res) {
@@ -40,9 +40,8 @@ router.get('/callback', (req, res) => {
         })
         .end((err, response) => {
 
-            access_token = response.body.access_token;
-
-            execute_api('balance');
+            originalApi.setAccessToken(response.body.access_token);
+            console.log('TOKEN: ' + originalApi.getAccessToken());
             res.send('<script>window.close();</script>');
         });
 });
@@ -55,51 +54,5 @@ router.get('/getCoin', (req, res) => {
             res.status(200).json(response.body);
         });
 });
-
-let resources = {
-    balance: {
-        method: 'get',
-        path: '/accounts/v1/balance'
-    },
-    balance_history: {
-        method: 'get',
-        path: '/accounts/v1/balance-history?date_from=20170623'
-    },
-    history: {
-        method: 'get',
-        path: '/accounts/v1/transaction-history'
-    }
-}
-
-let execute_api = name => {
-    let resource = resources[name];
-    
-    let action =
-        request
-            [resource.method](`${api_url}${resource.path}`)
-            .set('developer-key', developer_key)
-            .set('Authorization', access_token);
-
-    if ('headers' in resource)
-        for (let key in resource.headers)
-            action.set(key, resource.headers[key]);
-
-    if ('data' in resource)
-        action.send(resource.data);
-
-
-    action.end((err, res) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log(res.body);
-
-            if ('security_message' in res.body) {
-                resources.tef_confirm.headers.security_response = res.body.security_message
-            }
-        }
-    });
-};
 
 module.exports = router;
